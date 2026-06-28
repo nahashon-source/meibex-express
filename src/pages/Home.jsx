0656079af461e80a85bfda30768de5e80210189d
nahashon <nashonmwendwa0@gmail.com>
2026-06-28 08:47:30 +0300

    third commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center mt-10">
+        Meibex Express Ltd
+      </h1>
+    </div>
+  )
+}
+
+export default Home
